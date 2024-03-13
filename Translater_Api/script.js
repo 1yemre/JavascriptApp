dedf58e1264749845054826b49ba1cc6c058c46f
Emre  Enes Yenen <emreenesyenenn@gmail.com>
2024-03-13 17:40:32 +0300

    Translater

---
+++ Translater_Api/script.js
@@ -0,0 +1,15 @@
+const fromLang=document.querySelector("#from-lang");
+const ToLang=document.querySelector("#to-lang");
+
+for(let lang in languages)
+{
+     //console.log(lang,languages[lang]);
+     let option=
+     `<option value="${lang}">${languages[lang]}</option>`;
+     fromLang.insertAdjacentHTML("beforeend",option);
+     ToLang.insertAdjacentHTML("beforeend",option);
+
+
+     fromLang.value="tr-TR";
+     ToLang.value="en-GB";
+}
