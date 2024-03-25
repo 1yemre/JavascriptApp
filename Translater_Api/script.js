4c2b50f0a5dffc508871acacad35c2b4cb0b5d91
Emre  Enes Yenen <emreenesyenenn@gmail.com>
2024-03-25 23:53:27 +0300

    js_api

--- Translater_Api/script.js
+++ Translater_Api/script.js
@@ -1,5 +1,8 @@
 const fromLang=document.querySelector("#from-lang");
 const ToLang=document.querySelector("#to-lang");
+const btnTranslate=document.querySelector("#BtnTranslate");
+const fromText= document.querySelector("#from-text");
+const toText= document.querySelector("#to-text");
 
 for(let lang in languages)
 {
@@ -13,3 +16,18 @@ for(let lang in languages)
      fromLang.value="tr-TR";
      ToLang.value="en-GB"
 }
+
+btnTranslate.addEventListener("click",()=>{
+    let text=fromText.value;
+    let from=fromLang.value;
+    let to=ToLang.value;
+    const url=`https://api.mymemory.translated.net/get?q=${text}&langpair=${from}|${to}`;
+ 
+    fetch(url)
+       .then(res =>res.json())
+       .then(data=>
+          {
+               toText.value=data.responseData.translatedText;
+          });
+
+});
