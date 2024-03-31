037c6f68d338dfe43f1063c692589b01172d2c3b
Emre  Enes Yenen <emreenesyenenn@gmail.com>
2024-04-01 01:07:22 +0300

    Api

--- Search_app/app.js
+++ Search_app/app.js
@@ -1,20 +1,28 @@
-const profile=new Profile ();
-const searchProfile=document.querySelector("#searchProfile");
-const ui=new UI();
+const profile = new Profile();
+const ui = new UI();
+
+const searchProfile = document.querySelector('#searchProfile');
 
 
-searchProfile.addEventListener("keyup",(event)=>{
-   let text= event.target.value;
-   if(text!=='')
-   {
-     profile.getProfile(text)
-     .then(res=>{
-            if(res.profile.lenght===0)
-            {
+searchProfile.addEventListener('keyup',(event)=>{
+    ui.clear();
+    let text =  event.target.value;
 
-            }else{
-                 ui.showProfile(res.profile[0]);
-            }
-        })
-   }
+    if(text!==''){
+       profile.getProfile(text)
+        .then(res => {
+                if(res.profile.length === 0){
+                    ui.showAlert(text);
+                }else{                   
+                   ui.showProfile(res.profile[0]);
+                   ui.showTodo(res.todo);
+                }  
+            }) 
+        .catch(err=>{
+            ui.showAlert(text);
+        })  
+}
+        
+
 });
+
